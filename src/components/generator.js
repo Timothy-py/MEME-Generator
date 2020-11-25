92a1faf9573e6f26ed7ee9afe86544ddcd338943
Timothy-py <adeyeyetimothy33@gmail.com>
2020-11-26 00:02:47 +0100

    created components

---
+++ src/components/generator.js
@@ -0,0 +1,22 @@
+import React, {Component} from 'react'
+
+
+class Generator extends Component{
+
+    constructor(){
+        super()
+
+        this.state = {
+            
+        }
+    }
+
+    render(){
+        return(
+            <h2>MEME GENERATOR</h2>
+        )
+    }
+}
+
+
+export default Generator
---
+++ src/components/header.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react'
+
+
+function Header(params) {
+    
+    return(
+        <h2>HEADER</h2>
+    )
+
+}
+
+export default Header;
